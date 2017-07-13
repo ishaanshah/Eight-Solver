6382aa897c81c4d0b6d028c1271c3f01de6f9d19
Ishaan Shah <ishaan.n.shah@gmail.com>
2017-07-13 10:37:17 +0530

    Fixed get_current_state and added other functions

--- solver.py
+++ solver.py
@@ -1,26 +1,49 @@
 # Game logic and design by arnisritins. https://github.com/arnisritins/15-Puzzle.
 
+import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 # Initialize webdriver to communicate with webpage
 driver = webdriver.Chrome(r"C:\Users\ishaa\chromedriver.exe")
 driver.get("file:///F:/Projects/PycharmProjects/fifteen_solver/15-Puzzle/index.html")
 
-
-# structure to store coordinates
-class Coordinate:
-    def __init__(self, x, y, tile):
-        self.x = x
-        self.y = y
-        self.tile = tile
-
-
 # grid for puzzle
 grid = []
 
+# solved state of puzzle
+solved_grid = []
 
+# structure to store coordinates
+class Tile:
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+
+# structure for nodes
+class Node:
+    def __init__(self, config, parent, f, g, h):
+         self.config = config
+         self.parent = parent
+         self.f = f
+         self.g = g
+         self.h = h
+
+
+# find heuristics
+def heuristic(node):
+    dist = []
+    for tile in node.config:
+        for solved_tile in solved_grid:
+            if solved_tile.num == tile.num:
+                dist.append(str(manhattan_distance(tile, solved_tile)) + '-' + str(tile.num))
+
+    return dist
+
+# get current state of board
 def get_current_state():
+    # empty the list
+    grid[:] = []
     for i in range(0, 4):
         for j in range(0, 4):
             # get the tile from web page
@@ -29,21 +52,36 @@ def get_current_state():
             # check for empty tile
             if tile == '':
                 # set empty tile as 0
-                num_tile = 0
-            else:
-                # convert to integer
-                num_tile = int(tile)
+                tile = "0"
 
             # insert coordinates of tile in grid
-            grid.insert(num_tile, Coordinate(i, j, str(num_tile)))
+            grid.append(Tile(i, j, str(tile)))
+
+
+# find the taxicab distance between two points
+def manhattan_distance(start, end):
+    return abs(start.x - end.x) + abs(start.y - end.y)
+
+
+# submit a number
+def submit(tile):
+    input_box = driver.find_element_by_id("number")
+    input_box.send_keys(str(tile))
+    submit_btn = driver.find_element_by_id("submit")
+    submit_btn.click()
 
 
 def main():
-    get_current_state()
-    for i in range(16):
-        print("{}-{}-{}".format(grid[i].x, grid[i].y, grid[i].tile))
+    # create a solved board
+    num = 1
+    for i in range(4):
+        for j in range(4):
+            if i == 3 and j == 3:
+                solved_grid.append(Tile(i, j, 0))
+            else:
+                solved_grid.append(Tile(i, j, num))
+                num += 1
 
-    driver.close()
 
 if __name__ == "__main__":
     main()
