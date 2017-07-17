e5f66896bdb957f90a4110f4cdcf9bdbb9d01a5d
Ishaan Shah <ishaan.n.shah@gmail.com>
2017-07-17 13:10:15 +0530

    Changed solver.py to make it solve for 3x3 puzzle

--- solver.py
+++ solver.py
@@ -1,26 +1,28 @@
 # Game logic and design by arnisritins. https://github.com/arnisritins/15-Puzzle.
 
+import copy
 import time
 from operator import attrgetter
 from selenium import webdriver
 
 # Initialize webdriver to communicate with webpage
 driver = webdriver.Chrome(r"C:\Users\ishaa\chromedriver.exe")
-driver.get("file:///F:/Projects/PycharmProjects/fifteen_solver/15-Puzzle/index.html")
+driver.get("file:///F:/Projects/PycharmProjects/Eight_solver/8-Puzzle/index.html")
 
 # grid for puzzle
-grid = [[i for i in range(4)] for j in range(4)]
+grid = [[i for i in range(3)] for j in range(3)]
 
 # solved state of puzzle
-solved_grid = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,0]]
+solved_grid = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
 
 # structure for nodes
 class Node:
     def __init__(self, config, parent, f, g):
-         self.config = config
-         self.parent = parent
-         self.f = f
-         self.g = g
+        self.config = config
+        self.parent = parent
+        self.f = f
+        self.g = g
 
 
 def a_star():
@@ -31,88 +33,109 @@ def a_star():
     closed_list = []
     open_list = [start]
 
-    while len(open_set) != 0:
-        current = min(open_set, key = attrgetter("f"))
-
-        if win(current.config):
-            return True
-
-        open_set.remove(current)
-        closed_set.append(current)
-
+    while len(open_list) != 0:
+        current = min(open_list, key=attrgetter("f"))
+        open_list.remove(current)
         for neighbour in find_neighbours(current):
-            if neighbour in closed_set:
-                continue
+            if same_state(neighbour.config, end.config):
+                return True
 
-            if neighbour not in open_set:
-                open_set.append(neighbour)
+            for node in open_list:
+                if same_state(node.config, neighbour.config) and node.f < neighbour.f:
+                    continue
 
-            neighbour.parent = current
-            neighbour.f = neighbour.g + heuristic(neighbour)
+            for node in closed_list:
+                if same_state(node.config, neighbour.config) and node.f < neighbour.f:
+                    continue
 
+            open_list.append(neighbour)
+
+        closed_list.append(current)
     return False
 
 
-# finds the neighnours of current config
+# finds the neighbours of current config
 def find_neighbours(node):
     j = [j for j in node.config if 0 in j][0]
     zero_x = node.config.index(j)
     zero_y = j.index(0)
-    neigbours = []
-    neigbour = [i for i in node.config]
+    neighbours = []
 
-    if zero_x + 1 <= 3:
-        neigbour[zero_x + 1][zero_y], neigbour[zero_x][zero_y] = neigbour[zero_x][zero_y], neigbour[zero_x + 1][zero_y]
-        neigbours.append(neigbour)
+    if zero_x + 1 <= 2:
+        neighbour_1 = copy.deepcopy(node.config)
+        neighbour_1[zero_x + 1][zero_y], neighbour_1[zero_x][zero_y] = \
+            neighbour_1[zero_x][zero_y], neighbour_1[zero_x + 1][zero_y]
+        neighbours.append(Node(neighbour_1, node, heuristic(neighbour_1) + (node.g + 1), node.g + 1))
 
     if zero_x - 1 >= 0:
-        neigbour[zero_x - 1][zero_y], neigbour[zero_x][zero_y] = neigbour[zero_x][zero_y], neigbour[zero_x - 1][zero_y]
-        neigbours.append(neigbour)
+        neighbour_2 = copy.deepcopy(node.config)
+        neighbour_2[zero_x - 1][zero_y],  neighbour_2[zero_x][zero_y] =  \
+            neighbour_2[zero_x][zero_y],  neighbour_2[zero_x - 1][zero_y]
+        neighbours.append(Node(neighbour_2, node, heuristic(neighbour_2) + (node.g + 1), node.g + 1))
 
-    if zero_y + 1 <= 3:
-        neigbour[zero_x][zero_y + 1], neigbour[zero_x][zero_y] = neigbour[zero_x][zero_y], neigbour[zero_x][zero_y + 1]
-        neigbours.append(neigbour)
+    if zero_y + 1 <= 2:
+        neighbour_3 = copy.deepcopy(node.config)
+        neighbour_3[zero_x][zero_y + 1], neighbour_3[zero_x][zero_y] = \
+            neighbour_3[zero_x][zero_y], neighbour_3[zero_x][zero_y + 1]
+        neighbours.append(Node(neighbour_3, node, heuristic(neighbour_3) + (node.g + 1), node.g + 1))
 
     if zero_y - 1 >= 0:
-        neigbour[zero_x][zero_y - 1], neigbour[zero_x][zero_y] = neigbour[zero_x][zero_y], neigbour[zero_x][zero_y - 1]
-        neigbours.append(neigbour)
+        neighbour_4 = copy.deepcopy(node.config)
+        neighbour_4[zero_x][zero_y - 1], neighbour_4[zero_x][zero_y] = \
+            neighbour_4[zero_x][zero_y], neighbour_4[zero_x][zero_y - 1]
+        neighbours.append(Node(neighbour_4, node, heuristic(neighbour_4) + (node.g + 1), node.g + 1))
 
-    return neigbours
+    return neighbours
+
 
 # find heuristics
-def heuristic(node):
+def heuristic(config):
     dist = []
-    for i in range(16):
+    for i in range(9):
         j = [j for j in solved_grid if i in j][0]
         s_x = solved_grid.index(j)
         s_y = j.index(i)
-        k = [k for k in node.config if i in k][0]
-        n_x = node.config.index(k)
+        k = [k for k in config if i in k][0]
+        n_x = config.index(k)
         n_y = k.index(i)
         dist.append(abs(s_x - n_x) + abs(s_y - n_y))
 
     return sum(dist)
 
+
 # get current state of board
 def get_current_state():
     # empty the list
     grid[:] = []
-    for i in range(4):
+    for i in range(3):
         row = []
-        for j in range(4):
+        for j in range(3):
             tile = driver.find_element_by_id("cell-{}-{}".format(i, j)).text
             # check for black tile
             if tile == '':
-                tile ="0"
-            row.insert(j,int(tile))
+                tile = "0"
+            row.insert(j, int(tile))
 
         # insert row in grid
         grid.insert(i, row)
 
 
 def scramble():
-    driver.find_element_by_id("scramble").click()# wait for animation
-    time.sleep(0.6) # wait for animation
+    driver.find_element_by_id("scramble").click()  # wait for animation
+    time.sleep(0.6)  # wait for animation
+
+
+def same_state(grid1, grid2):
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if grid1[i][j] == grid2[i][j]:
+                count += 1
+
+    if count == 9:
+        return True
+    else:
+        return False
 
 
 # submit a number
@@ -121,21 +144,14 @@ def submit(tile):
     input_box.send_keys(str(tile))
     submit_btn = driver.find_element_by_id("submit")
     submit_btn.click()
-    time.sleep(0.3) # wait for animation
-
-
-def swap(tile1, tile2, grid):
-
-
-    return s_grid
+    time.sleep(0.3)  # wait for animation
 
 
 def main():
     scramble()
     get_current_state()
-    node = Node(grid, None, None, None)
-    for neighbour in find_neighbours(node):
-        print(neighbour)
+    print(a_star())
+    #driver.close()
 
 if __name__ == "__main__":
     main()
